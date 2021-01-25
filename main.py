a8d599f5d461ff482e2673af8411766271b9be7b
Achyuth K Kowshik <51409439+Achyuth17@users.noreply.github.com>
2021-01-25 10:40:36 +0530

    Made more efficient by adding grids

--- main.py
+++ main.py
@@ -1,38 +1,39 @@
 board=[
-    [1,1,1],
-    [1,1,1],
-    [1,1,1]
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
 ]
 
 def play(board):
+    grid={1:(0,0),2:(0,1),3:(0,2),4:(1,0),5:(1,1),6:(1,2),7:(2,0),8:(2,1),9:(2,2)}
     played=[]
     player_x=input("Enter the first player's name : ")
     player_y=input("Enter the second player's name : ")
 
-    while len(played)<9:
+    while len(played)<(len(board)*len(board[0])):
         print_board(board)
 
         print(f"Player {player_x} turn to play!!")
-        play_x=list(map(int,input("Enter the position at which u want to enter : ").split(" ")))
-        while play_x in played:
-            print("Invalid position !!")
-            play_x=list(map(int,input("Enter the position at which u want to enter : ").split(" ")))
+        play_x=int(input("Enter the grid number at which u want to play : "))
+        while play_x in played or (play_x<=0 or play_x>9):
+            print("Invalid position !! ")
+            play_x=int(input("Enter the grid number at which u want to play : "))
         played.append(play_x)
-        board[play_x[0]][play_x[1]]='X'
+        board[grid[play_x][0]][grid[play_x][1]]='X'
 
         print_board(board)
         if victory('X'):
             print(f"{player_x} is the winner !!!! ")
             break
 
-        if len(played)<9:
+        if len(played)<(len(board)*len(board[0])):
             print(f"Player {player_y} turn to play !! ")
-            play_y=list(map(int,input("Enter the position at which u want to enter : ").split(" ")))
-            while play_y in played :
+            play_y=int(input("Enter the grid number at which u want to play : "))
+            while play_y in played or (play_y<=0 or play_y>9) :
                 print("Invalid position !! ")
-                play_y=list(map(int,input("Enter the position at which u want to enter : ").split(" ")))
+                play_y=int(input("Enter the grid number at which u want to play : "))
             played.append(play_y)
-            board[play_y[0]][play_y[1]]='O'
+            board[grid[play_y][0]][grid[play_y][1]]='O'
 
             if victory('O'):
                 print(f"{player_y} is the winner !!!! ")
